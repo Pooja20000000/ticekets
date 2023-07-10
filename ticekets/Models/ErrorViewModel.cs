8d420700c5c534c3922b917c28f48ac97d20acf5
user <user@DESKTOP-F4GLAAU>
2023-07-10 05:08:34 -0700

    Add project files.

---
+++ ticekets/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ticekets.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
